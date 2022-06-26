ce7dc7139b61683ef89f99f4fe8471386d1a85ed
Sergei Gots <Serge.Gots@gmail.com>
2022-06-27 01:57:40 +0300

    Completed Lessons 1-5

---
+++ src/MyLinkedList.java
@@ -0,0 +1,105 @@
+import java.util.Arrays;
+
+public class MyLinkedList {
+
+    private Node head;
+    private int size;
+
+    public void add(int value) {
+        //if it's the first add within the list
+        if(head == null) {
+            this.head = new Node(value);
+        } else {
+            Node temp = head;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+
+            temp.setNext(new Node(value));
+        }
+
+        size++;
+    }
+
+    public int get(int index) {
+        int currentIndex = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            if(currentIndex == index) {
+                return temp.getValue();
+            } else {
+                temp = temp.getNext();
+                currentIndex++;
+            }
+        }
+
+        throw new IllegalArgumentException("Attempt to get an item with the index " + index
+                + " but size of the list = " + size);
+    }
+
+    public void remove(int index) {
+        int currentIndex = 0;
+        Node temp = head;
+        Node tempPrev = null;
+
+        while (temp != null) {
+            if( currentIndex == index) {
+                if(tempPrev != null) {
+                    tempPrev.setNext(temp.getNext());
+                } else {
+                    head = temp.getNext();
+                }
+                size--;
+                return;
+            } else {
+                tempPrev = temp;
+                temp = temp.getNext();
+                currentIndex++;
+            }
+        }
+
+
+        throw new IllegalArgumentException("Attempt to remove an item with the index " + index
+                + " but size of the list = " + size);
+    }
+
+    public String toString() {
+        int [] result = new int[size];
+
+        int idx = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            result[idx++] = temp.getValue();
+            temp = temp.getNext();
+        }
+
+        return Arrays.toString(result);
+    }
+
+    private static class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,142 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Sergei Gots
+ */
+public class Test {
+    enum ListsTestEnum {
+        ADD,
+        ADD_TO_THE_BEGIN,
+        GET,
+        REMOVE_FIRST
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(10);
+
+        System.out.println("myLinkedList = " + myLinkedList);
+        System.out.println(myLinkedList.get(0));
+        System.out.println(myLinkedList.get(1));
+        System.out.println(myLinkedList.get(2));
+        //Next line causes IllegalArgumentException:
+        //System.out.println(myLinkedList.get(3));
+
+        myLinkedList.remove(1);
+        System.out.println("myLinkedList = " + myLinkedList);
+
+        myLinkedList.remove(1);
+        System.out.println("myLinkedList = " + myLinkedList);
+
+        myLinkedList.remove(0);
+        System.out.println("myLinkedList = " + myLinkedList);
+
+
+    }
+    
+    void linkedListVsArrayListTest() {
+        
+        List <Integer> linkedList = new LinkedList<>();
+        List <Integer> arrayList = new ArrayList<>();
+
+        long timeLinkedList = 0;
+        long timeArrayList = 0;
+        int testNumber = 10;
+
+        linkedList.add(1);
+        linkedList.get(0);
+        linkedList.size();
+        linkedList.remove(0);
+
+        ListsTestEnum listsTest = ListsTestEnum.REMOVE_FIRST;
+        for (int i = 0; i < testNumber; i++) {
+            timeArrayList += measureTime(arrayList, listsTest);
+            timeLinkedList+= measureTime(linkedList, listsTest);
+
+            arrayList.clear();
+            linkedList.clear();
+        }
+
+        System.out.println("timeArrayList average = " + timeArrayList/testNumber);
+        System.out.println("timeLinkedList average = " + timeLinkedList/testNumber);
+    }
+
+
+    private static long measureTime(List<Integer>list, ListsTestEnum listsTest) {
+
+        long result;
+
+        if(listsTest == ListsTestEnum.GET
+                || listsTest == ListsTestEnum.REMOVE_FIRST) {
+
+            for (int i = 0; i <100000 ; i++) {
+                list.add(i);
+            }
+        }
+
+        long start = System.currentTimeMillis();
+
+        if(listsTest == ListsTestEnum.ADD) {
+            for (int i = 0; i < 10000000 ; i++) {
+                list.add(i);
+            }
+        } else if (listsTest == ListsTestEnum.GET) {
+            for (int i = 0; i < 100000; i++) {
+                list.get(i);
+            }
+        } else if (listsTest == ListsTestEnum.ADD_TO_THE_BEGIN) {
+            for (int i = 0; i <100000 ; i++) {
+                list.add(0, i);
+            }
+        } else if (listsTest == ListsTestEnum.REMOVE_FIRST) {
+            for (int i = 0; i <100000 ; i++) {
+                list.add(0, i);
+            }
+        }
+
+        long end = System.currentTimeMillis();
+
+        result = end - start;
+        System.out.println(list.getClass().getName() + ": " + result);
+        return result;
+    }
+
+    void intArrayListExample() {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        System.out.println(list);
+
+        System.out.println(list.get(0));
+        System.out.println(list.get(9));
+
+        System.out.println(list.size());
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(list.get(i)+ ", ");
+        }
+        System.out.println();
+
+        for (Integer x: list) {
+            System.out.print(x + ", ");
+        }
+        System.out.println();
+
+        list.remove(5);
+        System.out.println(list);
+    }
+
+    void intArrayExample() {
+        int[] x = new int[3];
+        for (int i = 0; i < 4; i++) {
+            x[i] = 1;
+        }
+    }
+}
