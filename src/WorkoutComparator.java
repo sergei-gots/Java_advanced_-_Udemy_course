53e8f7cdd10b59532aad5118a8edae558a994018
Sergei-Gots <Serge-Gots@gmail.com>
2022-07-06 01:30:50 +0300

    Completed Lesson 12 - Interface Comparator

--- src/WorkoutComparator.java
+++ src/WorkoutComparator.java
@@ -18,10 +18,25 @@ public class WorkoutComparator {
         //sorts corresponding the natural ordering
         Collections.sort(animals);
         System.out.println("animals = " + animals);
-        //sorts corresponding string's lenghts:
+        //sorts corresponding string's lengths:
         Collections.sort(animals, new StringLengthComparator());
         System.out.println("animals = " + animals);
 
+        Collections.sort(animals);
+        System.out.println("animals = " + animals);
+        Collections.sort(animals, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() > o2.length()){
+                    return 1;
+                } else if (o1.length() < o2.length()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        System.out.println("animals = " + animals);
 
         List<Integer> numbers = new ArrayList<>();
         numbers.add(5);
@@ -31,7 +46,69 @@ public class WorkoutComparator {
 
         Collections.sort(numbers);
         System.out.println("numbers = " + numbers);
+    
+        Collections.sort(numbers, new BackwardsIntegerComparator());
+        System.out.println("numbers = " + numbers);
 
+        Collections.sort(numbers);
+        System.out.println("numbers = " + numbers);
+        Collections.sort(numbers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(o1 > o2) {
+                    return -1;
+                } else if (o1 < o2) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        System.out.println("numbers = " + numbers);
+
+        //Let's sort our own Objects:
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(3, "Mike"));
+        people.add(new Person(2, "Katy"));
+        people.add(new Person(1, "Bob"));
+
+        //Let's sort people by their id:
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                if(o1.getId()>o2.getId()){
+                    return 1;
+                } else if (o1.getId()<o2.getId()) {
+                    return -1;
+                } else {
+                    return 0;
+                } 
+            }
+        });
+
+        System.out.println("people = " + people);
+    }
+}
+
+class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
 
@@ -43,6 +120,20 @@ class StringLengthComparator implements Comparator<String> {
             return 1;
         } else if (o1.length() < o2.length()) {
             return -1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+class BackwardsIntegerComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if(o1 > o2) {
+            return -1;
+        } else if (o1 < o2) {
+            return 1;
         } else {
             return 0;
         }
