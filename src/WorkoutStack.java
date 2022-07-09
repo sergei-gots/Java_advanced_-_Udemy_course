728ebfbc98588e7740c40efbb4b24a91ecd9014c
Sergei-Gots <Serge-Gots@gmail.com>
2022-07-09 21:05:55 +0300

    Lesson 15 "Class Stack" added

---
+++ src/WorkoutStack.java
@@ -0,0 +1,49 @@
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ * Lesson 15. Class Stack
+ */
+
+public class WorkoutStack {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        refill(stack);
+        
+        System.out.println("stack.peek() = " + stack.peek());
+        System.out.println("stack.peek() = " + stack.peek());
+
+        System.out.println("stack.pop() = " + stack.pop());
+        System.out.println("stack.pop() = " + stack.pop());
+
+        System.out.println("stack.peek() = " + stack.peek());
+
+        System.out.println("stack.pop() = " + stack.pop());
+        
+        try {
+            System.out.println("stack.pop() = " + stack.pop());
+        }
+        catch(EmptyStackException e) {
+            e.printStackTrace();
+        }
+        
+        refill(stack);
+
+        System.out.println("stack.empty() = " + stack.empty());
+
+        while(!stack.empty()){
+            System.out.println("stack.pop() = " + stack.pop());
+        }
+        System.out.println("stack.empty() = " + stack.empty());
+        
+    }
+
+    private static void refill(Stack<Integer> stack){
+        stack.push(5);
+        stack.push(3);
+        stack.push(1);
+    }
+}
