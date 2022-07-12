7ca175e56279efc0cb77bb23fa8c27de35c9ba73
Sergei-Gots <Serge-Gots@gmail.com>
2022-07-13 02:13:03 +0300

    Lesson 19 "Keyword sycnronised" added

---
+++ src/L19_Workout_Keyword_Synchronised.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022.
+ * To the Lesson 19 "Keyword Synchronised" of the video course "Java Advanced" by Neil Alishev (Udemy)
+ */
+
+public class L19_Workout_Keyword_Synchronised {
+
+    private int counter;
+
+    public static void main(String[] args) {
+        L19_Workout_Keyword_Synchronised workout = new L19_Workout_Keyword_Synchronised();
+        workout.doWork();
+    }
+
+    public void doWork() {
+        Thread thread1 = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                for(int i = 0; i<10000; i++) {
+                    counter++;
+                    if (counter == 5000) {
+                        System.out.println("thread1: counter == " + counter) ;
+                    }
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                for(int i = 0; i<10000; i++) {
+                    counter++;
+                    if (counter == 5000) {
+                        System.out.println("thread2: counter == " + counter);
+                    }
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            //with that we will wait until thread1 and thread2 will complete their work:
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("main thread: counter = " + counter);
+    }
+
+
+}
